90e08c73f07dbf530557adcdcee093ec8f4a0809
kirtan <kempire695@gmail.com>
2026-07-26 15:52:49 +0530

    Configure environment variables and fix email backend setup

--- food_app_backend/core/settings.py
+++ food_app_backend/core/settings.py
@@ -13,6 +13,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+from dotenv import load_dotenv  # <--- ADD THIS LINE HERE
+
+# Load environment variables from a local .env file
+load_dotenv()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,7 +146,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://housewives-invention-imagination-vector.trycloudflare.com",
     "http://127.0.0.1:5173",
-    "https://king-crave.vercel.app",
+    "https://king-crave.vercel.app",    
 ]
 
 # --- REST FRAMEWORK & JWT SETTINGS ---
@@ -157,10 +164,11 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kempire695@gmail.com'        
-EMAIL_HOST_PASSWORD = 'exnl pfty hsir ptor'             
-DEFAULT_FROM_EMAIL = 'Royal Crave <kempire695@gmail.com>'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
